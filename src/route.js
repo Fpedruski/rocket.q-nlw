5063d6b56457f1bb2dcc74997ec92a0dd65d1a8f
fpedruski <lipepock@gmail.com>
2021-06-25 15:53:48 -0300

    Setting server side and route

--- src/route.js
+++ src/route.js
@@ -1,9 +1,10 @@
-/
+/* Import express */
+const express = require("express");
 
-const express = require('express')
+/** Set to route express inside to be route + function */
+const route = express.Router();
 
-const route = express.Router()
+route.get("/", (req, res) => res.render("index"));
 
-route.get('/', (req, res) => res.render("index"))
-
-module.exports = route 
+/** ANCHOR Important to export the route comomand, this means the const route = express.Route() is exported automatically by the module and start to use the rendered index, we can fix another route for example with route.get(/home), (req, res) => res.render("home")) so the //./home request in the route.get */
+module.exports = route;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
+/** Import express */
+const express = require("express");
 
-const express = require('express')
-const route = require('./route')
+/** Import route to require ./route.js and use the file to work*/
+const route = require("./route");
 
-const server = express()
+/** Set the Server to be express + function */
+const server = express();
 
-server.use(route)
+/** Set a view engine to show the work, in another words who is in charge of the view engine?*/
+server.set("view engine", "ejs");
 
-server.listen(3000, () => console.log("RODANDO"))
+/* 
+! Normally the view is originated on the root directory from the application, this is a another way to redirect the path.
+? __dirname = src, in this case the __dirname is a variable that takes any path that is inside.
+? __dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file.
+! path.join, join the path to the next string inside the function, in this case "views".
+*/
+server.set("views", path.join(__dirname, "views"));
 
+/** Request express to use route */
+server.use(route);
 
-
-
+/** Set the server to listen the 3000 port and console a "working" */
+server.listen(3000, () => console.log("RODANDO"));
