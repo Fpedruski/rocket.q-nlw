4b838a9cd826f607135869d76dbb407ad81fc8f3
fpedruski <lipepock@gmail.com>
2021-06-29 21:41:23 -0300

    Adding ID on link

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,19 +2,50 @@ import Modal from './modal.js';
 
 const modal = Modal();
 
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
 const checkButtons = document.querySelectorAll('.actions a.check');
+
+
+/** Pulo do gato
+ * 
+ * 
+ * 
+ * 
+*/
+
+checkButtons.forEach(button => {
+	// Arrow function necessary as we are using the a module to parse the open( function )
+	button.addEventListener("click", handleClick);
+});
+
 const deleteButtons = document.querySelectorAll('.actions a.delete');
 
-checkButtons.forEach((button) => {
-	// Arrow function necessary as we are using the a module to parse the open( function )
-	button.addEventListener('click', (event) => {
-		// Abrir a modal
-		modal.open();
-	});
+deleteButtons.forEach(button => {
+	button.addEventListener('click', (event) => handleClick(event, false));
+
 });
 
-deleteButtons.forEach((button) => {
-	button.addEventListener('click', (event) => {
-		modal.open();
-	});
-});
+function handleClick(event, check = true) {
+	// Reitar # em links
+	event.preventDefault();
+	
+	const text = check ? "Marcar como lida" : "Excluir";
+
+	const roomId = document.querySelector("#room-id").dataset.id
+
+	const form = document.querySelector(".modal form")
+	form.setAttribute("action", `room/${roomId}/:question/:action`);
+
+	modalTitle.innerHTML = `${text} esta pergunta`;
+
+	modalDescription.innerHTML = 	`Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`;
+
+	modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+	check? modalButton.classList.remove("red") : modalButton.classList.add("red");
+	/** Abrir modal */
+	modal.open();
+};
+
+/** Min - 2:04 29jun. 2021 */
